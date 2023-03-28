9523befa4766669391b5dffdac604e4c623e0981
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2023-03-28 23:19:44 +0800

    Add files via upload

---
+++ cin与cout初步了解.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;//从左到右输入
+	cout << a<<" "<< b << c << d;//从左到右输出，基于智能性自主判断类型，空格输出方法
+
+	return 0;
+}
