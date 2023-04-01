a9656947d0add3412177b4a8f019faf92e238ba6
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2023-04-01 19:16:17 +0800

    Add files via upload

---
+++ happy fools’ day.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	char arr[] = { "Happy Fools' Day" };
+	char arr2[17] = { "                " };
+	int sz = sizeof(arr) / sizeof(arr[0]) - 1;
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		arr2[left] = arr[left];
+		arr2[right] = arr[right];
+		cout << arr2;
+		Sleep(1000);
+		system("cls");
+		left++;
+		right--;
+	}
+	printf("%s", arr2);
+
+	return 0;
+}
