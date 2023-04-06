be27ad4e70bc1d66fc4bc0b3cf2b553506bbffaf
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2023-04-06 23:24:12 +0800

    Add files via upload

---
+++ 类的简单使用.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class juice
+{
+private:
+	char name[19] = { "coco"};
+	int appraise = 5;
+public:
+	void set_name()
+	{
+		cin >> name;
+	}
+	void set_appraise()
+	{
+		cin >> appraise;
+	}
+	void show()
+	{
+		cout << "name:" << name << endl << "appraise:" << appraise;
+	}
+
+
+};
+
+
+int main()
+{
+	juice spirit;
+	spirit.set_name();
+	spirit.set_appraise();
+	spirit.show();
+
+
+
+	return 0;
+}
