26c5ad45c5baa950f7a9ceeb93e88577c13352c9
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2023-04-11 23:32:11 +0800

    Add files via upload

---
+++ 构造函数与析构函数练习.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+class juice
+{
+private:
+	char name[19] ;
+	int appraise ;
+public:
+
+	void init()
+	{
+		memset(name, '0', sizeof(name));
+		int a = 0;
+	}
+
+	juice(const char* arr,int a)
+	{
+		void init();
+		strcpy(name, arr);
+		appraise = a;
+		cout << "调用构造函数" << endl;
+	}
+	~juice()
+	{
+		cout << "调用析构函数";
+	}
+	void show()
+	{
+		cout << "name:" << name << endl << "appraise:" << appraise << endl;
+	}
+};
+
+int main()
+{
+	juice coco("coco",5);
+	coco.show();
+	return 0;
+}
